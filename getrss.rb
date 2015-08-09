295fc3b596f0076fb41fe5245a0654efde083848
ikatake <ikatake+fromgithub@gmail.com>
2015-08-09 10:31:59 +0900

    rssからブログのエントリ、タイトル、画像URI引っ張れるようにした

--- getrss.rb
+++ getrss.rb
@@ -1,10 +1,30 @@
 require 'rss'
+require 'pp'
 
 #はてなブログの/rssはRSS2.0を返すとのこと
 #
 
+def getPicSrc(description)
+  arr = description.split(" ")
+  arr.each do |e|
+    if /^src="(.*)"/ =~ e
+      return $1
+    end
+  end
+  return nil
+end
+
 
 uri = "http://wzm.hatenablog.com/rss"
 rss = RSS::Parser.parse(uri)
-p rss
+#p rss
+#pp rss
+#pp rss.channel.items
+
+rss.channel.items.each do |i|
+  puts i.link
+  puts i.title
+  puts getPicSrc(i.description)
+  puts	
+end
 
